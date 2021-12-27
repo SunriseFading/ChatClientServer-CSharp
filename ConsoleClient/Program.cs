fd786ba2fc9f4b236db4a8e688608d59864df487
Vendetta <vlad.bykov2004@mail.ru>
2021-12-27 21:07:46 +0300

    Complete console client

--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -4,15 +4,44 @@ namespace Messanger
 {
     class Program
     {
+        private static int MessageID;
+        private static string UserName;
+        private static MessangerClientAPI API = new MessangerClientAPI();
+
+        private static void GetNewMessages()
+        {
+            Message msg = API.GetMessage(MessageID);
+            while (msg != null)
+            {
+                Console.WriteLine(msg);
+                MessageID++;
+                msg = API.GetMessage(MessageID);
+            }
+        }
+
         static void Main(string[] args)
         {
-            Message msg = new Message("V", "Hey", DateTime.UtcNow);
-            string output = JsonConvert.SerializeObject(msg);
-            Console.WriteLine(output);
-            Message deserializedMsg = JsonConvert.DeserializeObject<Message>(output);
-            Console.WriteLine(deserializedMsg);
+            //Message msg = new Message("V", "Hey", DateTime.UtcNow);
+            //string output = JsonConvert.SerializeObject(msg);
+            //Console.WriteLine(output);
+            //Message deserializedMsg = JsonConvert.DeserializeObject<Message>(output);
+            //Console.WriteLine(deserializedMsg);
             //{ "UserName":"V","MessageText":"Hey","TimeStamp":"2021-12-22T19:21:31.6164227Z"}
             //V < 22.12.2021 19:21:31 >: Hey
+            MessageID = 1;
+            Console.WriteLine("Введите Ваше имя:");
+            UserName = Console.ReadLine();
+            string MessageText = "";
+            while (MessageText != "exit")
+            {
+                GetNewMessages();
+                MessageText = Console.ReadLine();
+                if (MessageText.Length > 1)
+                {
+                    Message Sendmsg = new Message(UserName, MessageText, DateTime.Now);
+                    API.SendMessage(Sendmsg);
+                }
+            }
         }
     }
 }
